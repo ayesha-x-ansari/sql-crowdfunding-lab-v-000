a2e7bfeb6f10660fbd748dca975f2b255b7f6c4f
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-05-14 01:31:23 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -25,7 +25,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "SELECT projects.title, sum(pledges.amount)  - projects.funding_goal AS amount_over_goal
+  "SELECT projects.title, SUM(pledges.amount)  - projects.funding_goal AS amount_over_goal
    FROM projects
    INNER JOIN pledges
    ON projects.id = pledges.project_id
@@ -34,7 +34,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-  "SELECT users.name, users.age, sum(pledges.amount)
+  "SELECT users.name,  sum(pledges.amount)
    FROM users
    INNER JOIN pledges
    ON users.id = pledges.user_id
