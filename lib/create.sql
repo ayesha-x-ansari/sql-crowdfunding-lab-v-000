b72fb5461e5ef95849a8c7fe4f36d8bf18f8e30e
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-05-14 01:28:17 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,9 +1,9 @@
 CREATE TABLE projects (id INTEGER PRIMARY KEY,
                        title TEXT,
                        category TEXT,
-                       funding_goal INTEGER,
-                       start_date TEXT,
-                       end_date TEXT);
+                       funding_goal FLOAT,
+                       start_date DATE,
+                       end_date DATE);
 
 CREATE TABLE users (id INTEGER PRIMARY KEY,
                     name TEXT,
